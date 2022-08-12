ba3ef5d93cbb266a186cbd44cacd4ecbe2adcd03
wisteria-soba <kaboman1123@gmail.com>
2022-08-12 16:45:40 +0900

    update template generator to craete build directory

--- mkcpt.sh
+++ mkcpt.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 mkdir $1/$2
+mkdir $1/$2/build
 for i in {a..h}
 do
 cp templates/ss.cpp $1/$2/$i.cpp
--- mkcptn.sh
+++ mkcptn.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 mkdir $1 
+mkdir $1/build
 for i in `seq 1 $2`
 do
 cp templates/ss.cpp $1/$i.cpp
